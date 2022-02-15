e45302a5b17bfcbe60b1ce6a87db96e24676bdfa
Md. Shakhawat Hossain Shahin <shakhawat.ch@gmail.com>
2022-02-15 22:34:31 +0600

    Initial Commit

---
+++ RIL/RILApp.swift
@@ -0,0 +1,17 @@
+//
+//  RILApp.swift
+//  RIL
+//
+//  Created by Shakhawat Hossain Shahin on 15/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

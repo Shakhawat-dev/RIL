acf3b100458027496251b247357a0710f422243d
Md. Shakhawat Hossain Shahin <shakhawat.ch@gmail.com>
2022-05-22 01:47:36 +0600

    - New Map View Added ✅

--- RIL/Core/Home/View/HomeView.swift
+++ RIL/Core/Home/View/HomeView.swift
@@ -15,7 +15,7 @@ struct HomeView: View {
     
     let screen = UIScreen.main.bounds
     
-    @State private var mapRegion: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 23.790552, longitude: 90.391769), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    
     
     init() {
         UITabBar.appearance().isHidden = true
@@ -24,131 +24,32 @@ struct HomeView: View {
     var body: some View {
         ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
             
-//            Color.green
-//            MapView()
-            
-//            Map(coordinateRegion: $mapRegion)
-            
-            MapView()
+            Map(coordinateRegion: $vm.mapRegion, showsUserLocation: true)
+
+            //            MapView()
             
             VStack {
                 
-                HStack {
-                    Button {
-                        withAnimation {
-                            showMenu.toggle()
-                        }
-                        
-                    } label: {
-                        Image(systemName: "line.3.horizontal")
-                            .padding()
-                    }
-                    
-                    Spacer()
-                    
-                    Image("logo_with_trans")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 48.0)
-                    
-                    Spacer()
-                    
-                    NavigationLink(destination: ProfileView()) {
-                        Image(systemName: "person")
-                            .padding()
-                            .background(
-                                Circle()
-                                    .fill(Color.red)
-                            )
-                            .clipShape(Circle())
-                            .padding(.trailing)
-                            .foregroundColor(.white)
-                        
-                    }
-                    
-                    
-                }
-                .background(
-                    ArcShape(curveSize: 40)
-                        .fill(Color.theme.background)
-                        .ignoresSafeArea(edges: .top)
-                )
+                headerSection
                 
                 Spacer()
                 
-                VStack {
-                    
-                    Spacer()
-                    
-                    HStack {
-                        
-                        NavigationLink {
-                            AllAlertsView()
-                        } label: {
-                            VStack {
-                                Image(systemName: "bell.fill")
-                                    .font(.title)
-                                    .foregroundColor(.red)
-                                
-                                Text("View All Alerts")
-                                    .font(.subheadline)
-                                    .foregroundColor(.red)
-                            }
-                        }
-
-                        Spacer()
-                        
-                        Button {
-                            // Do Something
-                        } label: {
-                            VStack {
-                                Image(systemName: "camera.fill")
-                                    .font(.title)
-                                
-                                Text("Add Details")
-                                    .font(.subheadline)
-                            }
-                        }
-
-                    }
-                    .padding()
-                    .background(
-                        ArcShape(curveSize: 64)
-                            .fill(Color.theme.background)
-                            .rotationEffect(Angle(degrees: 180))
-                    )
-                }
-                .frame(height: 170)
-                .overlay(alignment: .top, content: {
-                    Image("g_prev_ui")
-                        .resizable()
-                        .frame(width: 128, height: 128)
-                        .aspectRatio(contentMode: .fit)
-                })
-                .ignoresSafeArea(edges: .bottom)
+                bottomTabViewSection
+                    .overlay(alignment: .top, content: {
+                        Image("g_prev_ui")
+                            .resizable()
+                            .frame(width: 128, height: 128)
+                            .aspectRatio(contentMode: .fit)
+                    })
+                    .ignoresSafeArea(edges: .bottom)
             }
             
-            HStack(spacing: 0) {
-                SideMenuView()
-                    .offset(x: showMenu ? 0 : -screen.width)
-                
-                Spacer()
-                
-            }
-            .background(
-                Color.black.opacity(showMenu ? 0.2 : 0)
-                    .onTapGesture {
-                        withAnimation() {
-                            showMenu = false
-                        }
-                    }
-            )
-
+            sideMenu
+            
         }
         .ignoresSafeArea(edges: .bottom)
         .onAppear {
-            vm.locationManager.delegate = vm
-            vm.locationManager.requestWhenInUseAuthorization()
+            vm.checkIfLocationServiceIsEnabled()
         }
         
     }
@@ -161,3 +62,112 @@ struct HomeView_Previews: PreviewProvider {
 }
 
 var tabs = ["All Alerts", "Add Details"]
+
+extension HomeView {
+    var sideMenu: some View {
+        HStack(spacing: 0) {
+            SideMenuView()
+                .offset(x: showMenu ? 0 : -screen.width)
+            
+            Spacer()
+            
+        }
+        .background(
+            Color.black.opacity(showMenu ? 0.2 : 0)
+                .onTapGesture {
+                    withAnimation() {
+                        showMenu = false
+                    }
+                }
+        )
+    }
+    
+    var headerSection: some View {
+        HStack {
+            Button {
+                withAnimation {
+                    showMenu.toggle()
+                }
+                
+            } label: {
+                Image(systemName: "line.3.horizontal")
+                    .padding()
+            }
+            
+            Spacer()
+            
+            Image("logo_with_trans")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 48.0)
+            
+            Spacer()
+            
+            NavigationLink(destination: ProfileView()) {
+                Image(systemName: "person")
+                    .padding()
+                    .background(
+                        Circle()
+                            .fill(Color.red)
+                    )
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                    .foregroundColor(.white)
+                
+            }
+            
+            
+        }
+        .background(
+            ArcShape(curveSize: 40)
+                .fill(Color.theme.background)
+                .ignoresSafeArea(edges: .top)
+        )
+    }
+    
+    var bottomTabViewSection: some View {
+        VStack {
+            
+            Spacer()
+            
+            HStack {
+                
+                NavigationLink {
+                    AllAlertsView()
+                } label: {
+                    VStack {
+                        Image(systemName: "bell.fill")
+                            .font(.title)
+                            .foregroundColor(.red)
+                        
+                        Text("View All Alerts")
+                            .font(.subheadline)
+                            .foregroundColor(.red)
+                    }
+                }
+                
+                Spacer()
+                
+                Button {
+                    // Do Something
+                } label: {
+                    VStack {
+                        Image(systemName: "camera.fill")
+                            .font(.title)
+                        
+                        Text("Add Details")
+                            .font(.subheadline)
+                    }
+                }
+                
+            }
+            .padding()
+            .background(
+                ArcShape(curveSize: 64)
+                    .fill(Color.theme.background)
+                    .rotationEffect(Angle(degrees: 180))
+            )
+        }
+        .frame(height: 170)
+    }
+}
--- RIL/Core/Home/ViewModel/HomeViewModel.swift
+++ RIL/Core/Home/ViewModel/HomeViewModel.swift
@@ -8,16 +8,61 @@
 import Foundation
 import SwiftUI
 import CoreLocation
+import MapKit
+
+enum MapDetails {
+    static let startingLocation = CLLocationCoordinate2D(latitude: 23.790552, longitude: 90.391769)
+    static let defaultSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+}
 
 class HomeViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
-    @Published var locationManager = CLLocationManager()
+    @Published var locationManager: CLLocationManager?
     
     @Published var userLocation: CLLocation!
     @Published var userAddress = ""
     @Published var noLocation = false
     
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion(center: MapDetails.startingLocation, span: MapDetails.defaultSpan)
+    
+    func checkIfLocationServiceIsEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+//            locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        } else {
+            print("Show an alert letting them know location service is off")
+        }
+    }
+    
+    private func checkLocationAuthorization() {
+        guard let locationManager = locationManager else {
+            return
+        }
+        
+        switch locationManager.authorizationStatus {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Your location is restricted likely due to parantal control")
+        case .denied:
+            print("Location authorization denied")
+        case .authorizedAlways, .authorizedWhenInUse:
+            guard let location = locationManager.location else {
+                return
+            }
+            
+            mapRegion = MKCoordinateRegion(center: location.coordinate, span: MapDetails.defaultSpan)
+            break
+        @unknown default:
+            break
+        }
+
+    }
+    
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         // Checking Location Access...
+        /*
         switch manager.authorizationStatus {
         case .authorizedWhenInUse:
             print("authorized")
@@ -29,9 +74,12 @@ class HomeViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
         default:
             print("unknown")
             self.noLocation = false
-            locationManager.requestWhenInUseAuthorization()
+            locationManager?.requestWhenInUseAuthorization()
             
         }
+         */
+        
+        checkLocationAuthorization()
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {

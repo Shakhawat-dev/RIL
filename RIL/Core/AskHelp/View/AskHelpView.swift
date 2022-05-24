76e004193dc26e29b2e61cf91bd2f6b7df52a37e
MD. Shakhawat Hossain Shahin <Shakhawat.ch@gmail.com>
2022-05-24 15:44:13 +0600

    design completed

--- RIL/Core/AskHelp/View/AskHelpView.swift
+++ RIL/Core/AskHelp/View/AskHelpView.swift
@@ -21,68 +21,105 @@ struct AskHelpView: View {
         ZStack {
             Color.theme.background
             CustomNavBarView {
-                VStack {
-                    Text("Emergency Type")
-                        .font(.title3)
-                        .bold()
-                    
-                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]) {
-                        Button {
-                            // Do Something
-                            vm.emergencyType = .fuel
-                        } label: {
-                            EmergencyChipView(image: "fuel", title: "Fuel", isSelected: vm.emergencyType == .fuel)
-                        }
+                ScrollView {
+                    VStack(alignment: .center, spacing: 24.0) {
                         
-                        Button {
-                            // Do Something
-                            vm.emergencyType = .accident
-                        } label: {
-                            EmergencyChipView(image: "accident", title: "Accident", isSelected: vm.emergencyType == .accident)
-                        }
-                        
-                        Button {
-                            // Do Something
-                            vm.emergencyType = .damage
-                        } label: {
-                            EmergencyChipView(image: "bike", title: "Damage", isSelected: vm.emergencyType == .damage)
-                        }
-                        
-                        Button {
-                            // Do Something
-                            vm.emergencyType = .other
-                        } label: {
-                            EmergencyChipView(image: "emm", title: "Other", isSelected: vm.emergencyType == .other)
-                        }
-
-                    }
-                    .padding(.horizontal, 32)
-                    
-                    VStack {
-                        Text("Add a short note")
-                            .font(.title3)
-                            .bold()
-                        
-                        TextEditor(text: $vm.shortNoteText)
-                    }
-                    
-                    VStack {
-                        Text("Add a Image(Optional)")
-                            .font(.title3)
-                            .bold()
-                             
-                        Button {
-                            // Do Something
-                        } label: {
-                            Image("placeholder")
-                                .resizable()
-                                .scaledToFit()
+                        VStack(alignment: .leading) {
+                            Text("Emergency Type")
+                                .font(.title3)
+                                .bold()
                             
+                            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]) {
+                                Button {
+                                    // Do Something
+                                    vm.emergencyType = .fuel
+                                } label: {
+                                    EmergencyChipView(image: "fuel", title: "Fuel", isSelected: vm.emergencyType == .fuel)
+                                }
+                                
+                                Button {
+                                    // Do Something
+                                    vm.emergencyType = .accident
+                                } label: {
+                                    EmergencyChipView(image: "accident", title: "Accident", isSelected: vm.emergencyType == .accident)
+                                }
+                                
+                                Button {
+                                    // Do Something
+                                    vm.emergencyType = .damage
+                                } label: {
+                                    EmergencyChipView(image: "bike", title: "Damage", isSelected: vm.emergencyType == .damage)
+                                }
+                                
+                                Button {
+                                    // Do Something
+                                    vm.emergencyType = .other
+                                } label: {
+                                    EmergencyChipView(image: "emm", title: "Other", isSelected: vm.emergencyType == .other)
+                                }
+
+                            }
+                            .padding(.horizontal, 48)
+                        }
+                        
+                        
+                        VStack(alignment: .leading) {
+                            Text("Add a short note")
+                                .font(.title3)
+                                .bold()
+                            
+                            TextEditor(text: $vm.shortNoteText)
+                                .padding()
+                                .background(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .fill(.white)
+                                )
+                                .frame(height: 64)
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            Text("Add a Image(Optional)")
+                                .font(.title3)
+                                .bold()
+                                 
+                            Button {
+                                // Do Something
+                            } label: {
+                                Image("placeholder")
+                                    .resizable()
+                                    .scaledToFit()
+                                
+                            }
+
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Text("ASK FOR HELP NOW")
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(
+                                    RoundedRectangle(cornerRadius: 4)
+                                )
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "phone.fill")
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .padding()
+                                .background(
+                                    Circle()
+                                )
                         }
 
                     }
                 }
                 
+                .padding()
                 
             }
         }

d3769b7a027b239aaa5f63f9913add1f2421f0a9
MD. Shakhawat Hossain Shahin <Shakhawat.ch@gmail.com>
2022-05-30 16:21:34 +0600

    Registration done

---
+++ RIL/Models/RegistrationViewModel.swift
@@ -0,0 +1,81 @@
+//
+//  RegistrationViewModel.swift
+//  RIL
+//
+//  Created by Shakhawat Hossain Shahin on 30/5/22.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+
+class RegistrationViewModel: ObservableObject {
+    
+    @Published var name: String = ""
+    @Published var email: String = ""
+    @Published var phone: String = ""
+    @Published var address: String = ""
+    @Published var password: String = ""
+    
+    @Published var emergency1: String = ""
+    @Published var emergency2: String = ""
+    @Published var emergency3: String = ""
+    
+    private var authVM = AuthViewModel()
+    
+    //    @Published var newAlertText: String = ""
+    
+    private lazy var databasePath: DatabaseReference? = {
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return nil
+        }
+        let ref = Database.database()
+            .reference()
+            .child("users")
+        return ref
+    }()
+    
+    private let encoder = JSONEncoder()
+    private let decoder = JSONDecoder()
+    
+    
+    //      func stopListening() {
+    //        databasePath?.removeAllObservers()
+    //      }
+    
+    func createUser() {
+        
+//        authVM.signUp(emailAddress: email, password: password)
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResults, error in
+            guard let uid = authResults?.user.uid else { return }
+            
+            self.postUser(uid: uid)
+        }
+    }
+    
+    private func postUser(uid: String) {
+        guard let databasePath = databasePath else {
+            return
+        }
+        
+        guard let key = databasePath.childByAutoId().key else {
+            return
+        }
+        
+        if name.isEmpty || phone.isEmpty || password.isEmpty {
+            return
+        }
+        
+        let profileInfo = ProfileModel(address: address, emergencyNumber: [emergency1, emergency2, emergency3], mail: email, name: name, notificationId: "", ph: phone, pp: "", userId: uid, status: 0)
+        
+        do {
+            let data = try encoder.encode(profileInfo)
+            let json = try JSONSerialization.jsonObject(with: data)
+            databasePath.child(uid)
+                .setValue(json)
+        } catch {
+            print("an error occurred", error)
+        }
+    }
+}
